4fb14ea39330047347d7255b9806f41d0597f208
wild0rez <mau.dirollo@stud.uniroma3.it>
2019-09-24 13:57:56 +0200

    Aggiunto un file per Project Management e aggiornato lo script.

---
+++ ProjectEvolution.md
@@ -0,0 +1,21 @@
+# Todo and Specification about my thesis project
+
+## 1. Project Introduction
+
+---
+#### THINGS I NEED TO KNOW
+
+- [ ]  Understand how to parse an XML file.
+- [ ]  Understand how to parse cli argument.
+- [ ]  Beause the core of the script will need recursion, read something about it!
+- [ ]  How to do permutation in python (generators/yield maybe).
+
+
+#### THINGS I NEED TO DO
+- [ ]  Try to write an essay about the functionality of the script.
+  - [ ]  Brief history about the research context.
+  - [ ]  Which are the functionality of the script.
+  - [ ]  What a user has to do for use it.
+  - [ ]  Which are the expected outputs.
+- [ ]  Write some specs about the evolutional flow of the script.
+- [ ]  Write, not in a deep way, Uses Case and Diagrams.
--- parser.py
+++ parser.py
@@ -3,17 +3,27 @@ import xml.etree.ElementTree as ET
 
 # Create an argparser that will help the user with some instruction
 def createArgumentParser():
-    parser = argparse.ArgumentParser(description='This script will read an AGLibrary compatible XML file and will create all the possibile permutation of the multiple routes field and than will be saved in differentes files.')
-    parser.add_argument('-f', '--file', nargs=1, help='Insert the name or path of the file.')
+    parser = argparse.ArgumentParser(description='This script take as input an XML AGLibrary compatible file and will produce all the possible permutation of the multiple routing.')
+    parser.add_argument('-f', '--file', required=True, nargs=1, help='Insert the name or path of the file.')
     args = parser.parse_args()
     return args.file[0]
 
 # Open a file passed as parameter and will return the root of the XML
 def readFileAndCreateRoot(file_name):
-    tree = ET.parse(file_name)
-    root = tree.getroot()
+    try:
+        tree = ET.parse(file_name)
+        root = tree.getroot()
+        return root
+    except:
+        print("No file named: " + file_name + " has been found.")
+        return None
 
-    return root
+# Testing ArgumentParse and XML Parse
+def printRoot():
+    openedFile = createArgumentParser()
+    if(readFileAndCreateRoot(openedFile) is not None):
+        root = readFileAndCreateRoot(openedFile)
+        for child in root:
+            print(child.tag)
 
-
-print (createArgumentParser())
+printRoot()
