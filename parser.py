405532764bb3160b9384ae982b0bab40975995b0
wild0rez <mau.dirollo@stud.uniroma3.it>
2019-10-06 18:48:31 +0200

    Implementato metodo per scrivere su XML
    Da migliorare, per ora salva solo su un file (inutile)

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import argparse
-from itertools import permutations, product
+import os
+import shutil
+from itertools import permutations, product, chain
 import xml.etree.ElementTree as ET
 
 # Create an argparser that will help the user with some instruction
@@ -18,8 +20,18 @@ def readFileAndCreateTree(file_name):
         print("No file named: " + file_name + " has been found.")
         return None
 
+def createDirectory():
+    path = './output'
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+
 def extractDataFromXML():
     openedFile = createArgumentParser()
+    shutil.copyfile(openedFile, './output/{0}'.format(os.path.basename('template.xml')))
     arl = [] # Creo una lista che ospiterà tutte le liste degli id_time_profile
     if(readFileAndCreateTree(openedFile) is not None):
         tree = readFileAndCreateTree(openedFile)
@@ -34,6 +46,7 @@ def extractDataFromXML():
 
 def allThePerms(data):
     count  = 0
+    tmp = []
     # Dato che, come parametro gli sto passando una lista di liste, mi calcolo tutte le possibili permutazioni
     # attraverso map che prende due parametri, list che consente appunto di creare una lista e l'altro
     # permutations(sublist) che, attraverso la funzione permutations calcola tutte le possibili permutazioni
@@ -42,7 +55,32 @@ def allThePerms(data):
     perms = [list(map(list,permutations(sublist))) for sublist in data]
     for data in product(*perms):
         count += 1 
-        print(list(data))
-    print(count)
+        tmp.append(data)
+    return tmp
 
-allThePerms(extractDataFromXML())
+def saveToSingleFile(subl):
+    tree = readFileAndCreateTree('./output/template.xml')
+    for (a, e) in zip(tree.findall('.//job//multiple_routings_list_elem//id_time_profile'), subl):
+        print(a.tag, e)
+        #a.text = e
+        #tree.write('./output/cazzone'+str(count)+'.xml')
+        #count -= 1
+    print('== end ==')
+
+
+def exportModifiedXML():
+    createDirectory()
+    allThePermsTuples = allThePerms(extractDataFromXML())
+    for subl in allThePermsTuples:
+        out = list(chain(*subl)) 
+        saveToSingleFile(out)
+
+def main():
+   exportModifiedXML()
+   # with open("./output/tuples.txt", "w+") as f:
+   #     f.write('\n'.join(str(subl) for subl in allThePerms(extractDataFromXML())))
+   #     f.close()
+
+
+if __name__ == "__main__":
+    main()
