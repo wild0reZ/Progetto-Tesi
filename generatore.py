e1e701dabc48da35e9a26ccd1d2578c3838b13ab
wild0rez <maurodirollo@gmail.com>
2020-06-15 12:54:30 +0200

    Sistemato progetto

--- generatore.py
+++ generatore.py
@@ -2,32 +2,41 @@ import argparse
 import os
 import shutil
 import itertools
+import random
 from lxml import etree
 
+_DEBUG = False
+
 # Create an argparser that will help the user with some instruction
+
+
 def createArgumentParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', required=True, nargs=1, help='Insert the name or path of the file.')
+    parser.add_argument('-f', required=True, nargs=1,
+                        help='Insert the name or path of the file.')
     args = parser.parse_args()
     return args.f[0]
 
 # Open a file passed as parameter and will return the root of the XML
+
+
 def readFileAndCreateTree(file_name):
     try:
         tree = etree.parse(file_name)
-        return tree 
+        return tree
     except:
         print("No file named: " + file_name + " has been found.")
         return None
+
 
 def createDirectory():
     path = './output'
     try:
         os.mkdir(path)
     except OSError:
-        print ("Creation of the directory %s" % path)
+        print("Creation of the directory %s" % path)
     else:
-        print ("Successfully created the directory %s " % path)
+        print("Successfully created the directory %s " % path)
 
 def prevision():
     openedFile = createArgumentParser()
@@ -41,41 +50,44 @@ def prevision():
         for node in tree.iter('job'): # Cerco all'interno del file XML tutti i tag job
             for elem in node.iter('multiple_routings_list_elem'): # Cerco tutti i child di job con tag multiple_routings_list_elem
                 jobNumber += 1
-                #print('Job number ', jobNumber)
                 mrlrNumber += 1
-                #print('Multi Route number', mrlrNumber)
                 itpNumber = 0
                 for a in elem.iter(): # Inizio a cercare gli id_time_profile
                     if(a.tag == 'id_time_profile'):
                         itpNumber += 1
-                        #print('Id Time Profile number ', itpNumber)
                 mrlList.append(itpNumber)
+    
     for x in mrlList:
         ris = ris * x
     print('La previsione del numero di file prodotti è pari a: ', ris)
-    for i in range(0, 101):
-        perc = (ris*i)/100
-        print('Prendendo il ', i, ' %', ' devo salvare: ', perc, ' file')
-    #return arl
+    return ris
 
 
 def extractDataFromXML():
     openedFile = createArgumentParser()
-    shutil.copyfile(openedFile, './output/{0}'.format(os.path.basename('template.xml')))
-    arl = [] # Creo una lista che ospiterà tutte le liste degli id_time_profile
+    numberOfItp = []
+    shutil.copyfile(
+        openedFile, './output/{0}'.format(os.path.basename('template.xml')))
+    arl = []  # Creo una lista che ospiterà tutte le liste degli id_time_profile
     if(readFileAndCreateTree(openedFile) is not None):
         tree = readFileAndCreateTree(openedFile)
-        for node in tree.iter('job'): # Cerco all'interno del file XML tutti i tag job
-            for elem in node.iter('multiple_routings_list_elem'): # Cerco tutti i child di job con tag multiple_routings_list_elem
-                tmp = [] # Inizializzo una lista vuota che conterrà tutti gli id_time_profile del singolo multiple_routings_list_elem
-                for a in elem.iter(): # Inizio a cercare gli id_time_profile
+        # Cerco all'interno del file XML tutti i tag job
+        for node in tree.iter('job'):
+            # Cerco tutti i child di job con tag multiple_routings_list_elem
+            for elem in node.iter('multiple_routings_list_elem'):
+                tmp = []  # Inizializzo una lista vuota che conterrà tutti gli id_time_profile del singolo multiple_routings_list_elem
+                for a in elem.iter():  # Inizio a cercare gli id_time_profile
                     if(a.tag == 'id_time_profile'):
-                        tmp.append(a.text) # Li salvo all'interno della lista
-                arl.append(tmp) # Salvo la lista all'interno della lista di tutti i multiple_routings_list_elem
+                        tmp.append(a.text)  # Li salvo all'interno della lista
+                numberOfItp.append(len(tmp))
+                # Salvo la lista all'interno della lista di tutti i multiple_routings_list_elem
+                arl.append(tmp)
+    if _DEBUG: print(numberOfItp); print(len(numberOfItp))
     return arl
 
 def rotate(l, n):
     return l[n:] + l[:n]
+
 
 def allThePermsRotation(data):
     listOfAllRotations = []
@@ -85,16 +97,45 @@ def allThePermsRotation(data):
         for i in range(len(sublist)):
             allThePerms.append(rotate(sublist, i))
         listOfAllRotations.append(allThePerms)
+        if _DEBUG: print(listOfAllRotations)
     for data in itertools.product(*listOfAllRotations):
         allRotationsCartesianProduct.append(data)
-    return allRotationsCartesianProduct 
+    return allRotationsCartesianProduct
+
+def randomRotation(data):
+    tmpList = []
+    tmp = []
+    partialRandomRotation = []
+    count = 0
+    while(count < 100):
+        for subl in data:
+            tmpList = subl.copy()
+            randStart = tmpList[random.randrange(len(tmpList))]
+            if(not(tmpList[0] == randStart)):
+                firstValue = subl[0]
+                tmpList.remove(firstValue)
+                tmpList.remove(randStart)
+                tmpList.insert(0, randStart)
+                tmpList.append(firstValue)
+            tmp.append(tmpList)
+        
+        if(not(tmpList in partialRandomRotation)):
+            partialRandomRotation.append(tmp)
+            count += 1
+        tmp = []
+    return partialRandomRotation
+
 
 def exportModifiedXML():
     createDirectory()
     path = str(createArgumentParser())
     rest, fileName = os.path.split(path)
     export = fileName.split('.')[0]
-    allThePermsTuples = allThePermsRotation(extractDataFromXML())
+    allThePermsTuples = []
+    if (prevision() <= 100):
+        allThePermsTuples = allThePermsRotation(extractDataFromXML())
+    else:
+        allThePermsTuples = randomRotation(extractDataFromXML())
     count = 0
     for subl in allThePermsTuples:
         tree = readFileAndCreateTree('./output/template.xml')
@@ -111,7 +152,7 @@ def removeSpaces():
     print('Removing unnecessary spaces. Wait')
     for filename in os.listdir(os.chdir('./output')):
         # first get all lines from file
-        with open(filename, 'r+') as f:
+        with open(filename, 'r+', encoding='latin-1') as f:
             lines = f.readlines()
         # remove spaces
         print('Working on file ', count)
@@ -126,7 +167,6 @@ def removeSpaces():
 def main():
     exportModifiedXML()
     removeSpaces()
-    #prevision()
 
 if __name__ == "__main__":
     main()
